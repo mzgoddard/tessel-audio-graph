9b1c85d43a36306a97359c7b772f730263f904a7
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-01-09 03:23:16 -0500

    Health check tcp urls with tcp

--- stream.js
+++ stream.js
@@ -1,64 +1,90 @@
-// import {request} from 'http';
+const stream = require('stream');
 const url = require('url');
 const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 const request = require('http').request;
 const net = require('net');
 
-function hc(uri) {
-  process.stdout.write('.');
-  // console.log(arguments);
+function healthCheck(uri) {
   return new Promise((resolve, reject) => {
     const parsed = url.parse(uri);
-    // console.log(parsed);
-    const req = request({
-      method: 'get',
-      hostname: parsed.hostname,
-      port: parsed.port || 80,
-      path: '/',
-    });
-    req.on('response', function(message) {
-      // console.log('response');
-      // console.log(message);
-      message.setEncoding('utf8');
-      message.on('error', function(err) {
-        console.error(err);
-        reject(err);
+
+    let req;
+    if (parsed.protocol === 'http:') {
+      req = request({
+        method: 'get',
+        hostname: parsed.hostname,
+        port: 80,
+        path: '/',
       });
-      message.on('data', function() {});
-      message.on('end', function() {
-        // console.log('hc');
-        resolve();
+      req.on('response', function(message) {
+        message.setEncoding('utf8');
+        message.on('error', reject);
+        message.on('data', function() {});
+        message.on('end', function() {resolve();});
       });
-    });
-    req.on('error', function(err) {
-      console.error(err);
-      reject(err);
-    });
+    }
+    else {
+      req = net.createConnection({port: parsed.port, host: parsed.hostname});
+      req.on('data', function() {});
+      req.on('end', function() {resolve();});
+    }
+
+    req.on('error', reject);
     req.end();
-    // , (err, response, body) => {
-    //   console.log(err, response, body);
-    //   if (err) {
-    //     console.error(err);
-    //     reject(err);
-    //   }
-    //   else {
-    //     console.log('hc');
-    //     resolve();
-    //   }
-    // });
   });
 }
 
-function loop(cmd, cmdOpts, uri, tcpPort) {
-  // console.log(arguments);
-  return hc(uri)
-  // return Promise.resolve()
-  .then(() => {
-    return new Promise((resolve, reject) => {
-      const child = spawn(cmd[0], cmd.slice(1), cmdOpts);
-      const parsed = url.parse(uri);
-      // console.log('connecting');
-      const req = request({
+function lowBufferChild(cmd, cmdOpts) {
+  child = spawn(cmd[0], cmd.slice(1), cmdOpts);
+
+  let bufferIndex = 0;
+  buffer = new Buffer(96000);
+  const chunk = new Buffer(192);
+
+  const originalStdout = child.stdout;
+  originalStdout.pause();
+
+  let eof = false;
+  child.stdout = new stream.Readable();
+  child.stdout._read = function() {
+    if (eof) {
+      this.push(null);
+      return;
+    }
+  };
+
+  function writeLoop() {
+    try {
+      if (eof) {
+        child.stdout.push(null);
+        return;
+      }
+      let data;
+      while ((data = originalStdout.read(192)) && child.stdout.push(data));
+    }
+    catch (e) {console.error(e);}
+    setTimeout(writeLoop, 0);
+  };
+  writeLoop();
+
+  child.on('exit', function() {eof = true;});
+  child.on('error', function() {eof = true;});
+  originalStdout.on('end', function() {eof = true;});
+  originalStdout.on('error', function() {eof = true;});
+
+  return child;
+}
+
+function streamToServer(options, childStdout) {
+  let buffer, child;
+  return new Promise((resolve, reject) => {
+    const parsed = url.parse(options.url);
+
+    let req;
+    if (parsed.protocol === 'http:') {
+      console.log('connecting over http');
+      req = request({
         method: 'POST',
         hostname: parsed.hostname,
         port: parsed.port || 80,
@@ -67,135 +93,94 @@ function loop(cmd, cmdOpts, uri, tcpPort) {
           'Transfer-Encoding': 'chunked',
         },
       });
-      // const req = net.createConnection({port: tcpPort, host: parsed.hostname});
-      req.setNoDelay(true);
-      // child.stdout.pipe(req);
-      let bufferIndex = 0;
-      let buffer = new Buffer(96000);
-      const chunk = new Buffer(192);
-      // child.stdout.on('data', function(data) {
-      //   if (!buffer) {return;}
-      //   // console.log(`send ${data.length} bytes`);
-      //   // req.write(chunk);
-      //   for (let i = 0; i < data.length; i++) {
-      //     buffer[bufferIndex++] = data[i];
-      //   }
-      //   // let i = 0;
-      //   // for (; i < (bufferIndex / chunk.length | 0); i++) {
-      //   //   for (let j = 0; j < chunk.length; j++) {
-      //   //     chunk[j] = buffer[i * chunk.length + j];
-      //   //   }
-      //   //   req.write(chunk);
-      //   // }
-      //   // for (let j = i * chunk.length; j < bufferIndex; j++) {
-      //   //   buffer[j - i * chunk.length] = buffer[j];
-      //   // }
-      //   // console.log(`sent ${i} chunks. ${bufferIndex - i * chunk.length} left over`);
-      //   // bufferIndex -= i * chunk.length;
-      // });
-      child.stdout.pause();
-      function writeLoop() {
-        try {
-        let data = child.stdout.read(192);
-        if (data) {
-          // process.stdout.write(data.length.toString(16) + ' ');
-          for (let i = 0; i < data.length; i++) {
-            buffer[bufferIndex++] = data[i];
-          }
-        }
-        }
-        catch (e) {console.error(e);}
-        if (buffer && bufferIndex > chunk.length) {
-          for (let j = 0; j < chunk.length; j++) {
-            chunk[j] = buffer[j];
-          }
-          for (let j = 0; j < bufferIndex - chunk.length; j++) {
-            buffer[j] = buffer[j + chunk.length];
-          }
-          bufferIndex -= chunk.length;
-          req.write(chunk, writeLoop);
-          // console.log(Date.now());
-        }
-        else if (buffer) {
-          setTimeout(writeLoop, 0);
-        }
-      }
-      writeLoop();
-      console.log('start write loop');
-      // child.stdout.on('end', function() {
-      //   req.end();
-      // });
-      // child.stderr.pipe(process.stderr);
-      req.on('response', function(message) {
-        message.on('data', function() {});
-        message.on('end', function() {
-          buffer = null;
-          child.kill();
-          reject();
-        });
-        message.on('error', function(err) {
-          console.error(err);
-          buffer = null;
-          child.kill();
-          reject();
-        });
-      });
-      req.on('error', function(err) {
-        console.error(err);
-        buffer = null;
-        child.kill();
-        reject(err);
-      });
-      child.on('exit', function() {
-        buffer = null;
-        req.end();
-        reject();
-      });
+    }
+    else {
+      console.log('connecting over tcp');
+      req = net.createConnection({port: parsed.port, host: parsed.hostname});
+    }
+
+    req.setNoDelay(true);
+
+    let bufferIndex = 0;
+    buffer = new Buffer(96000);
+    const chunk = new Buffer(192);
+
+    let connected = false;
+    req.on('response', function(message) {
+      console.log('connected');
+      message.on('data', function() {if (!connected) {connected = true; console.log('connected');}});
+      message.on('end', function() {reject();});
+      message.on('error', reject);
     });
+    req.on('data', function() {if (!connected) {connected = true; console.log('connected');}});
+    req.on('end', function() {reject();});
+    req.on('error', reject);
+    req.on('close', reject);
+
+    childStdout.pipe(req);
+  });
+}
+
+function loop(cmd, cmdOpts, uri, tcpPort) {
+  return healthCheck(uri)
+  .then(() => {
+    console.log('found server');
+    const child = lowBufferChild(cmd, cmdOpts);
+    const closeHandle = (err) => {
+      if (err) {console.error(err);}
+      console.log('disconnected');
+      try {
+        child.kill();
+      } catch (e) {}
+    };
+    return streamToServer({url: uri}, child.stdout)
+    .then(closeHandle, closeHandle);
   })
-  .then(() => loop(cmd, cmdOpts, uri), () => {
+  .then(() => loop(cmd, cmdOpts, uri), err => {
+    console.log('attempting after error', err);
     return new Promise(resolve => setTimeout(resolve, 2000))
     .then(() => loop(cmd, cmdOpts, uri));
   });
 }
 
-if (!process.argv[2] || process.argv[2] === 'all' || process.argv[2] === 'spotify') {
-loop(
-  // ['sox', '|sox -d -p', '-r', '48000', '-b', '16', '-c', '2', '-t', 'raw', '-'],
-  ['sox', '-d', '-b', '16', '-r', '48000', '-c', '2', '-t', 'raw', '-'],
-  // ['sox', '-d', '-t', 'raw', '-'],
-  {
-    stdio: ['ignore', 'pipe', 'ignore'],
-    env: Object.assign({}, process.env, {
-      // AUDIODEV: 'Spotify',
-      // AUDIODEV: 'Chrome',
-    }),
-  },
-  // 'http://127.0.0.1:7777/music'
-  // 'http://Tessel-02A32DCC461B.local/music'
-  'http://cello.local/music'
-  // 'http://10.31.0.125/music'
-  // 'http://10.0.1.20/music'
-  ,
-  7777
-);
+var commands = {
+  sox: ['sox', '-d', '-b', '16', '-r', '48000', '-c', '2', '-t', 'raw', '-'],
+};
+
+if (process.argv.length < 3) {
+  console.log(`
+    usage: node stream <AUDIO_DEVICE> <SERVER_ENDPOINT>
+
+      AUDIO_DEVICE is any recordable input device on the system.
+
+      SERVER_ENDPOINT is either a http or tcp url like:
+        - http://mdns-server.local/post_endpoint
+        - tcp://mdns-server.local:port_number
+  `);
+  return;
 }
 
-if (process.argv[2] === 'all' || process.argv[2] === 'chrome') {
-loop(
-  ['sox', '|AUDIODEV=Chrome sox -d -p', '-r', '48000', '-b', '16', '-c', '2', '-t', 'raw', '-'],
-  // ['sox', '-d', '-b', '16', '-r', '48000', '-c', '2', '-t', 'raw', '-'],
-  // ['sox', '-d', '-t', 'raw', '-'],
-  {
-    stdio: ['ignore', 'pipe', 'ignore'],
-    env: Object.assign({}, process.env, {
-      AUDIODEV: 'Chrome',
-      // AUDIODEV: 'Hijack',
-    }),
-  },
-  'http://Tessel-02A32DCC461B.local/chrome'
-  // 'http://10.0.1.20/chrome'
-  ,
-  7778
-);
+function checkSox() {
+  return new Promise((resolve, reject) => {
+    exec('sox --version', function(err) {
+      if (err) {return reject(err);}
+      else {resolve();}
+    });
+  });
 }
+
+checkSox()
+.then(function() {
+  loop(
+    commands.sox,
+    {
+      stdio: ['ignore', 'pipe', 'ignore'],
+      env: Object.assign({}, process.env, {
+        AUDIODEV: process.argv[2],
+      }),
+    },
+    process.argv[3]
+  );
+}, function() {
+  console.error("Can't find sox. Please install it with 'apt' or 'brew'.");
+});
